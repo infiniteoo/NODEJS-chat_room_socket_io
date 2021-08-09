fe0ea744c279c9fcea5fce332cbe6860535c0b68
Troy Dorman <troydorman@gmail.com>
2021-08-09 13:50:56 -0600

    added a couple more util functions: userLeave and getRoomUsers

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const formatMessage = require("./utils/messages");
-const { userJoin, getCurrentUser } = require("./utils/users");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
