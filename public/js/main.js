25d238d71b052be404907fa086f9f5c7000ee3a4
Troy Dorman <troydorman@gmail.com>
2021-08-09 12:44:09 -0600

    clear chat/text input after submission and call focus method on it

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,10 @@ chatForm.addEventListener("submit", (e) => {
 
   // emit message to server
   socket.emit("chatMessage", msg);
+
+  // clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 // output message to DOM
