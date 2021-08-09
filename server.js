970bdc716aed53a6e92c67a040044bcf96d63d7b
Troy Dorman <troydorman@gmail.com>
2021-08-09 11:46:02 -0600

    incorporated socket.io into our server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 const path = require("path");
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// run when a client connects
+io.on("connection", (socket) => {
+  console.log("new web socket connection.");
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
